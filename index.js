f03b8cb739241971b24f1ca05302cbf7b6576dea
Ilya <illybesson3@gmail.com>
2023-01-02 02:37:19 +0200

    Home work

---
+++ index.js
@@ -0,0 +1,6 @@
+const userName = prompt('What is your name?') ;
+alert(`Hello ${userName} !How are you?` ) ;
+// let someText = "Hello"
+// let someTextText = " ! How are you? "
+// console.log(alert);
+// console.log();
