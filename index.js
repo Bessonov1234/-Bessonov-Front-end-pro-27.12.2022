536909785c987f3db5a09ee9777d51aee397bc16
Ilya <illybesson3@gmail.com>
2023-01-02 16:18:06 +0200

    Home work3.1

---
+++ index.js
@@ -0,0 +1,9 @@
+const userName = prompt('What is your name?') ;
+const userAge = prompt('How old are you?') ;
+const userCity = prompt('Where are you from?') ;
+let Hello = "Hello!"
+let Age = "years old you,"
+let City = "and you from"
+
+
+console.log(`${Hello} ${userName} ${userAge} ${Age} ${City} ${userCity}`);
