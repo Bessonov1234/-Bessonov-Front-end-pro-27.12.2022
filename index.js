0e06b0c4519f4358e23dcdb356041a3e60d39b54
Ilya <illybesson3@gmail.com>
2023-01-09 21:21:21 +0200

    Home work4

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const userName = prompt('What is your name?') ;
-const userAge = prompt('How old are you?') ;
-const userCity = prompt('Where are you from?') ;
-let Hello = "Hello!"
-let Age = "years old you,"
-let City = "and you from"
+const userName = prompt("What is your name?");
+const userAge = +prompt("How old are you?");
+const userSmoke = confirm("Do you smoke?");
+let adultSmoke = `Привіт ${userName}, що ж, ти сам обрав цей шлях...`;
+let adult = `Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`;
+let notAdultSmoke = `Привіт ${userName}! А батьки знають про це?`;
+let notAdult = `Привіт ${userName}! Правильно, зараз головне добре вчитися!`;
 
-
-console.log(`${Hello} ${userName} ${userAge} ${Age} ${City} ${userCity}`);
+if (userAge < 18) {
+  if (userSmoke === true) {
+    console.log(notAdultSmoke);
+  } else {
+    console.log(notAdult);
+  }
+}
+if (userAge > 18) {
+  if (userSmoke === false) {
+    console.log(adult);
+  } else {
+    console.log(adultSmoke);
+  }
+}
