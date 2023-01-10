c6ae85af62990e97818b8f0cbfd243bc52186c9b
Ilya <illybesson3@gmail.com>
2023-01-10 18:00:24 +0200

    Hw5Js

---
+++ index.js
@@ -0,0 +1,35 @@
+let question = prompt(`What action you want to do`);
+let one = +prompt(`enter a number`);
+let two = +prompt(`enter a number`);
+
+let add = -`+`;
+let sub = -`-`;
+let mult = -`+`;
+let div = -`/`;
+// add, sub, mult, div;
+
+if (question === -`+`) {
+  console.log(one + two);
+}
+if (question === `sub`) {
+  console.log(one - two);
+}
+if (question === `mult`) {
+  console.log(one * two);
+}
+if (question === `div`) {
+  console.log(one / two);
+}
+
+if (question === `add`) {
+  console.log(one + two);
+}
+if (question === `sub`) {
+  console.log(one - two);
+}
+if (question === `mult`) {
+  console.log(one * two);
+}
+if (question === `div`) {
+  console.log(one / two);
+}
