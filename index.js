942a33450eec61e2037316ce59ecc51349c31dd9
Bessonov Ilya <illybesson3@gmail.com>
2023-01-14 16:37:54 +0430

    Commit Hw6

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
-let question = prompt(`What action you want to do 
- (add(+));   (sub(-));    (mult(*));   (div(/));`);
-let one = +prompt(`Give the number`);
-let two = +prompt(`Give the number`);
+let age = +prompt(`How old are you ?`);
+let city = prompt(`Where are you from `);
+let sport = prompt(`What is your favorite sport ?`);
+let sportmanVolleyball = `Мартиненко Олександр Євгенович`;
+let sportmanFootball = `Лионе́ль Андре́с Ме́сси Куччитти́ни`;
+let sportmanWeight = `Чумак Дмитро Віталійович`;
 
-// 
-// ! Знаки
-if (question === `+`) {
-  console.log(one + two);
-}else{
-if (question === `-`) {
-    console.log(one - two);
-  }else{
-    if (question === `*`) {
-    console.log(one * two);
-    }else{
-    if (question === `/`) {
-    console.log(one / two);
-    }
-  }
+// ! Частина 1
+
+if (city == `Лондон` || city == `Київ` || city == `Вашингтон`) {
+  alert(`"Круто тобі ${age}! Ти живеш у столиці ${city} `);
+  console.log(`"Круто тобі ${age}! Ти живеш у столиці ${city} `);
+} else {
+  if (city) {
+    alert(`Тобі ${age}! Ти живеш у місті ${city} `);
+    console.log(` Тобі ${age}! Ти живеш у місті ${city} `);
+  } else {
   }
 }
 
-// ! Слова
-if (question === `add`) {
-  console.log(one + two);
-}else{
-if (question === `sub`) {
-    console.log(one - two);
-  }else{
-    if (question === `mult`) {
-    console.log(one * two);
-    }else{
-    if (question === `div`) {
-    console.log(one / two);
+// ! Частина 2
+
+if (sport === `Волейбол`) {
+  alert(`"Круто! Хочеш стати як ${sportmanVolleyball}? `);
+  console.log(`"Круто! Хочеш стати як ${sportmanVolleyball}?`);
+} else {
+  if (sport === `Футбол`) {
+    alert(`"Круто! Хочеш стати як ${sportmanFootball}? `);
+    console.log(`"Круто! Хочеш стати як ${sportmanFootball}?`);
+  } else {
+    if (sport === `Важка атлетика`) {
+      alert(`"Круто! Хочеш стати як ${sportmanWeight}? `);
+      console.log(`"Круто! Хочеш стати як ${sportmanWeight}?`);
     }
   }
-  }
 }
-  
 
+if (city === null || city == ``) {
+  alert(`Шкода, що Ви не захотіли ввести свій(ю) Місце`);
+  console.log(`Шкода, що Ви не захотіли ввести свій(ю) Місце`);
+}
 
-// if (question === `add`) {
-//   console.log(one + two);
-// }
-// if (question === `sub`) {
-//   console.log(one - two);
-// }
-// if (question === `mult`) {
-//   console.log(one * two);
-// }
-// if (question === `div`) {
-//   console.log(one / two);
-// }
+if (age == 0 || age === false) {
+  alert(`Шкода, що Ви не захотіли ввести свій(ю) рік `);
+  console.log(`Шкода, що Ви не захотіли ввести свій(ю) рік `);
+}
+
+if (sport === null || sport == ``) {
+  alert(`Шкода, що Ви не захотіли ввести свій(ю) улюблений вид спорту `);
+  console.log(`Шкода, що Ви не захотіли ввести свій(ю) улюблений вид спорту `);
+}
