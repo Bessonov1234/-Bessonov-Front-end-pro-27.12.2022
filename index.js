0f7b4ae944d4386acdc75e3b9eaf453c10839358
Bessonov Ilya <illybesson3@gmail.com>
2023-02-24 18:12:38 +0430

    HW_1

--- index.js
+++ index.js
@@ -1,118 +1,72 @@
-// ! 1
+class Student {
+  constructor(name, lastname, age, assessments) {
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+    this.assessments = assessments;
+    this.Attendance = [];
+  }
 
-function sumArray(arr) {
-  let result = [];
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] == "number") {
-      result.push(arr[i]);
-      count++;
+  present() {
+    const present = this.Attendance;
+    present.push(true);
+    if (present.length <= 25) {
+      console.log(present);
+    } else {
+      // console.log('end');
+      console.log(present.unshift());
     }
+    return present;
   }
-  console.log(result);
-  return console.log(
-    result.reduce((accamulator, team) => accamulator + team) / count
-  );
-}
-
-let array = ["string", 5, 6, true, undefined, null, {}, 5, [5, 13, 75, 52]];
-
-sumArray(array);
-
-// ! 2
-function doMath(
-  x = +prompt(" Число №1"),
-  znak = prompt(" Що ви хочете зробити з цими числами ? "),
-  y = +prompt(" Число №2")
-) {
-  if (
-    isNaN(x) ||
-    isNaN(y) ||
-    y == null ||
-    x == null ||
-    y == undefined ||
-    x == undefined
-  ) {
-    return doMath();
-  }
-  switch (znak) {
-    case "-":
-      console.log(x - y);
-      alert(x - y);
-      break;
-    case "+":
-      console.log(x + y);
-      alert(x + y);
-
-      break;
-
-    case "/":
-      console.log(x / y);
-      alert(x / y);
-
-      break;
-    case "%":
-      console.log(x % y);
-      alert(x % y);
-
-      break;
-
-    case "^":
-      console.log(x ** y);
-      alert(x ** y);
-      break;
-    default:
-      console.log("default");
-      alert("Вибачте ви щось ввели не правильно. Будь-ласка повторіть спробу!");
-      return doMath();
-  }
-}
-doMath();
-
-// ! 3
-
-function massFilling (array, arrayTwo, Filling1, Filling2){
-  const arr1 = new Array(array);
-  const arr2 = new Array(arrayTwo);
-  arr1.push(arr2);
-  console.log(arr1)
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] == undefined){
-      arr1.pop()
-    return massFilling ()
-  }else{
-    arr1.push(Filling1)
-  }
-  }
-
-  
-}
-
-
-let Fill = prompt('Чим бажаєте заповнити одномірний масиву?');
-let Fill2 = prompt('Чим бажаєте заповнити двомірний масиву?');
-let LongArr1 = +prompt('Довжина двомірного масиву');
-let LongArr2 = +prompt('Довжина одномірного масиву');
-
-massFilling (LongArr1, LongArr2)
-
-// ! 4
-function removeSymbols(string, letters) {
-  // let stringArray = string.split("");
-  const result = [];
-
-  for (let i = 0; i < string.length; i += 1) {
-    if (!letters.includes(string[i])) {
-      result.push(string[i]);
+  absent() {
+    const present = this.Attendance;
+    present.push(false);
+    if (present.length <= 25) {
+      console.log(present);
+    } else {
+      present.unshift();
     }
+    return present;
   }
-  let newString = result.join("");
-  return newString;
+  summary() {
+    // this.absent();
+    // this.present()
+    // let summaryAbsent = this.absent;
+    // let summaryPresent = this.present;
+    let summaryPt = this.Attendance;
+    let count = 0;
+    for (let i = 0; i <= summaryPt.length; i++) {
+      if ((summaryPt[i] = true)) {
+        count++;
+      }
+    }
+    console.log(summaryPt);
+  }
+  //  average() {
+  //     let result = this.assessments.split(" ");
+  //     let count = 0;
+  //     let arr = [];
+  //     result.forEach((el) => {
+  //       arr.push(el - 0);
+  //       count++;
+  //     });
+  //    return arr.reduce((accamulator, team) => accamulator + team) / count;
+  //   }
+
+  //   ageStudents() {
+  //     console.log(`Me name is ${this.name},and I am ${this.age} years old . `);
+  //   }
+
+  //   averageScoreStudents() {
+  //     console.log(`I have average score  ${this.average()}`);
+  //   }
 }
 
-let string = prompt("Напишіть текст");
-let input = prompt("Які букви хочете видалити");
-let arrays = input.split("");
-let stringArray = string.split("");
-console.log(removeSymbols(string, arrays));
+let petro = new Student("Petro", "Mogila", 20, "10 20 30 50 60 50 60 5 20");
+
+// petro.ageStudents();
+petro.present();
+petro.present();
+petro.absent();
+
+petro.summary();
