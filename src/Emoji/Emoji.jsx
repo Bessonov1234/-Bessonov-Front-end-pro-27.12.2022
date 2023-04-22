cf399dd9d948164eea70755abf3e03fd1ba32509
Bessonov Ilya <illybesson3@gmail.com>
2023-04-22 21:12:12 +0430

    (not_done)

---
+++ src/Emoji/Emoji.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import EmojiLisener from "./components";
+
+class Emoji extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <>
+        <EmojiLisener />
+      </>
+    );
+  }
+}
+export default Emoji
