0aa861eebbbdaea6a1323d70aee92665ef76f0bf
Bessonov Ilya <illybesson3@gmail.com>
2023-04-27 19:53:22 +0430

    hw-20

--- src/components/Rank/Emoji/Emoji.jsx
+++ src/components/Rank/Emoji/Emoji.jsx
@@ -3,30 +3,18 @@ import classes from "./Emoji.module.scss";
 import { useState } from "react";
 import Smiles from "./Smiles/Smiles";
 import { v4 as uuidv4 } from "uuid";
-console.log(smilesData.smileys);
 const EmojiRank = () => {
   const [emojiData, setEmojiData] = useState(smilesData);
   const listemojiData = emojiData.smileys;
-  let listemojiData1 = emojiData;
-  const handleInc = (id) => {
-    listemojiData.forEach((el) => {
-      console.log(listemojiData);
-      if (id == el.id) {
-         el.count++
-      }
-    });
-    setEmojiData(listemojiData)
-  };
 
-  handleInc = (id) => {
-    const emoji = listemojiData.map((smiley) => {
-      if (smiley.id == id) {
-        console.log(smiley.count++);
-       return smiley.count++;
-      }
-      return smiley;
+  const handleInc = (id) => {
+    console.log(true);
+    const copyList = [...listemojiData];
+    console.log(copyList);
+    copyList.map((el) => {
+      return { ...el };
     });
-    setEmojiData(emoji);
+    setEmojiData(copyList);
   };
 
   return (
@@ -36,7 +24,7 @@ const EmojiRank = () => {
           <div className={classes.display}>
             <div className={classes.display__wrap__for__smiles}>
               <div className={classes.display__wrap__for__smiles__wrap}>
-                {smilesData.smileys.map((el) => {
+                {listemojiData.map((el) => {
                   return (
                     <Smiles
                       key={uuidv4()}
