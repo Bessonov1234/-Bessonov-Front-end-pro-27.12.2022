cc1275c873454d1b4679ab959ba210e5f8419576
Bessonov Ilya <illybesson3@gmail.com>
2023-06-01 09:14:43 +0430

    HW26

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import LoginForm from "./components/LoginForm"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
 
-function App() {
 
-  return (
-    <>
-      <LoginForm />
-    </>
-  )
-}
+const App = () => {
+	return (
+		<div className='container bg-white p-4 mt-5'>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+			<TodoList />
+		</div>
+	);
+};
 
-export default App
+export default App;
